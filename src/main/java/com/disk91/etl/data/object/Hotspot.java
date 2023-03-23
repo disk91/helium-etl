ded86fdc00774359a067bdbbb5c5968d2fd11dcd
Paul <disk_91@hotmail.com>
2023-03-23 01:58:39 +0100

    fix hotspot id as uniq to prevent write conflict that can create multiple from cache (should not but happen when bug)

--- src/main/java/com/disk91/etl/data/object/Hotspot.java
+++ src/main/java/com/disk91/etl/data/object/Hotspot.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Document(collection = "etl_hotspots")
 @CompoundIndexes({
-        @CompoundIndex(name = "hotspotId", def = "{'hotspotId' : 'hashed'}"),
+        @CompoundIndex(name = "hotspotId", def = "{'hotspotId' : 1}", unique = true),
         @CompoundIndex(name = "hotspotId_Id", def = "{'hotspotId' : 1, 'id' : 1}")
 })
 @Sharded(shardKey = { "hotspotId", "id" }, shardingStrategy = ShardingStrategy.RANGE)
