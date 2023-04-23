34a97808978a792821251a46fb34684ebab2edda
Paul <disk_91@hotmail.com>
2023-04-24 00:06:36 +0200

    improve async commit for hotspots

--- src/main/java/com/disk91/etl/service/HotspotCacheAsync.java
+++ src/main/java/com/disk91/etl/service/HotspotCacheAsync.java
@@ -1,14 +1,20 @@
 package com.disk91.etl.service;
 
+import com.disk91.etl.data.object.Beacon;
 import com.disk91.etl.data.object.Hotspot;
 import com.disk91.etl.data.repository.HotspotsRepository;
+import com.mongodb.WriteConcern;
+import com.mongodb.bulk.BulkWriteResult;
 import fr.ingeniousthings.tools.Now;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.BulkOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -38,6 +44,9 @@ public class HotspotCacheAsync {
         return processed;
     }
 
+    @Autowired
+    MongoTemplate mongoTemplate;
+
     @Async
     public void processAsyncCacheCommit(List<Hotspot> list) {
         synchronized (running) {
@@ -52,13 +61,37 @@ public class HotspotCacheAsync {
         total = list.size();
         processed=0;
         log.info("Hotspot Async commit starts "+total+" elements");
+
+        ArrayList<Hotspot> _hotspotDelayedInsert = new ArrayList<>();
         for ( Hotspot h : list ) {
-            hotspotsRepository.save(h);
+
+            // Improve write with bulk insert
+            if ( _hotspotDelayedInsert.size() < 1000 ) {
+                _hotspotDelayedInsert.add(h);
+            } else {
+                mongoTemplate.setWriteConcern(WriteConcern.W1.withJournal(true));
+                BulkOperations bulkInsert = mongoTemplate.bulkOps(BulkOperations.BulkMode.UNORDERED, Beacon.class);
+                for ( Hotspot _h : _hotspotDelayedInsert ) {
+                    bulkInsert.insert(_h);
+                }
+                BulkWriteResult bulkWriteResult = bulkInsert.execute();
+                _hotspotDelayedInsert.clear();
+            }
+            //hotspotsRepository.save(h);
             processed++;
             if ( (Now.NowUtcMs() - lastProgress) > 30_000) {
                 log.debug("Hotspot Async commit - progress "+getProgress()+"%");
                 lastProgress = Now.NowUtcMs();
             }
+        }
+        if ( _hotspotDelayedInsert.size() > 0 ) {
+            mongoTemplate.setWriteConcern(WriteConcern.W1.withJournal(true));
+            BulkOperations bulkInsert = mongoTemplate.bulkOps(BulkOperations.BulkMode.UNORDERED, Beacon.class);
+            for ( Hotspot _h : _hotspotDelayedInsert ) {
+                bulkInsert.insert(_h);
+            }
+            BulkWriteResult bulkWriteResult = bulkInsert.execute();
+            _hotspotDelayedInsert.clear();
         }
         list.clear();
         log.debug("Hotspot Async commit done");
