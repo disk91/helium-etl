3c2e273097a0d1fdedabba79fbb81ba032be26fa
Paul <disk_91@hotmail.com>
2024-03-01 11:08:17 +0100

    missing } .. strange

--- src/main/java/com/disk91/etl/data/repository/HotspotsIndexRepository.java
+++ src/main/java/com/disk91/etl/data/repository/HotspotsIndexRepository.java
@@ -29,7 +29,7 @@ public interface HotspotsIndexRepository extends MongoRepository<HotspotIndex, S
         " [ [" +
         "    [?0, ?1], [?0, ?3], [?2, ?3], [?2, ?1], [?0, ?1]" +
         " ] ]" +
-        "} } }")
+        "} } } }")
     public Slice<HotspotIndex> findByPositionNearbyBox(double bottomLeftLongitude, double bottomLeftLatitude, double topRightLongitude, double topRightLatitude, Pageable p);
 
 }
