54a108e5283d9018b2b8c21be5a3d221439e1950
Paul <disk_91@hotmail.com>
2024-04-02 21:30:24 +0200

    fix first entry init in witness history

--- src/main/java/com/disk91/etl/data/object/Hotspot.java
+++ src/main/java/com/disk91/etl/data/object/Hotspot.java
@@ -381,6 +381,8 @@ public class Hotspot implements ClonnableObject<Hotspot> {
             wh.setTimeRef(hRef);
             wh.setCountWitnesses(1);
             if ( selected ) wh.setSeletedWitness(1);
+            wh.setTotLateMs(respLate);
+            if ( unselectedReason != WIT_ERROR_NONE ) wh.addCause(unselectedReason);
             this.witnessesHistory.add(wh);
 
             // need to clean the older one ?
