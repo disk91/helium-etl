b7f13ddf327321cac423a4f11e7165d4f4f45b92
Paul <disk_91@hotmail.com>
2024-04-21 08:18:29 +0200

    missing removal

--- src/main/java/fr/ingeniousthings/tools/ObjectCache.java
+++ src/main/java/fr/ingeniousthings/tools/ObjectCache.java
@@ -19,7 +19,7 @@
  */
 package fr.ingeniousthings.tools;
 
-import org.openjdk.jol.info.GraphLayout;
+//import org.openjdk.jol.info.GraphLayout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
